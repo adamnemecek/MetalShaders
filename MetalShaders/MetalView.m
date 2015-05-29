2a12d38aab6f1ea0cc633c98ae15de5a47c38944
Lixuan Zhu <lixuan@production.pro>
2015-05-29 02:09:27 -0400

    Add documentations --Tyler

--- MetalShaders/MetalView.m
+++ MetalShaders/MetalView.m
@@ -16,15 +16,24 @@
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     if ((self = [super initWithCoder:aDecoder])) {
-        self.metalLayer = (CAMetalLayer *)self.layer;
-        self.device = MTLCreateSystemDefaultDevice();
-        self.metalLayer.device = self.device;
-        self.metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
-
-        [self buildVertexBuffers];
-        [self buildPipeLine];
+        [self setUp];
     }
     return self;
+}
+
+- (void)setUp {
+    [self setUpMetalLayer];
+    [self buildVertexBuffers];
+    [self buildPipeLine];
+}
+
+- (void)setUpMetalLayer {
+    // Get GPU
+    self.device = MTLCreateSystemDefaultDevice();
+    // Configure MetalLayer
+    self.metalLayer.device = self.device;
+    self.metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    self.metalLayer = (CAMetalLayer *)self.layer;
 }
 
 - (void)buildVertexBuffers {
@@ -52,19 +61,23 @@
 }
 
 - (void)buildPipeLine {
+    // Get library which contains all metal functions
     id<MTLLibrary> library = [self.device newDefaultLibrary];
     id<MTLFunction> vertexFunction = [library newFunctionWithName:@"vertex_main"];
     id<MTLFunction> fragmentFunction = [library newFunctionWithName:@"fragment_main"];
 
+    // Create Pipeline Descriptor
     MTLRenderPipelineDescriptor *pipelineDescriptor = [MTLRenderPipelineDescriptor new];
     pipelineDescriptor.vertexFunction = vertexFunction;
     pipelineDescriptor.fragmentFunction = fragmentFunction;
     pipelineDescriptor.colorAttachments[0].pixelFormat = self.metalLayer.pixelFormat;
 
+    // Create Pipeline
     self.pipleline = [self.device newRenderPipelineStateWithDescriptor:pipelineDescriptor
                                                                  error:NULL];
 }
 
+// Display Link
 - (void)didMoveToWindow {
     [super didMoveToSuperview];
     if (self.superview) {
@@ -77,40 +90,41 @@
     }
 }
 
+// Call back for each display link signal
 - (void)onDisplayLinkCalled:(CADisplayLink *)displayLink {
     [self redraw];
 }
 
+// Metal Rendering Pass
 - (void)redraw {
+    // Acquire Drawable
     id<CAMetalDrawable> drawable = [self.metalLayer nextDrawable];
     id<MTLTexture> texture = drawable.texture;
 
+    // Configure render pass
     MTLRenderPassDescriptor *passDescriptor = [MTLRenderPassDescriptor renderPassDescriptor];
     passDescriptor.colorAttachments[0].texture = texture;
     passDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
     passDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
     passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(1.0, 0.5, 0.5,1.0);
 
+    // Metal Commands
+    // encoder -> buffer -> queue
     id<MTLCommandQueue> commandQueue = [self.device newCommandQueue];
     id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
     id<MTLRenderCommandEncoder> encoder = [commandBuffer renderCommandEncoderWithDescriptor:passDescriptor];
+    // Configure encoder
     [encoder setRenderPipelineState:self.pipleline];
     [encoder setVertexBuffer:self.positionBuffer offset:0 atIndex:0];
     [encoder setVertexBuffer:self.colorBuffer offset:0 atIndex:1];
+
+    // start issue commands
     [encoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:6 instanceCount:1];
     [encoder endEncoding];
 
+    // upload to GPU and render the pass
     [commandBuffer presentDrawable:drawable];
     [commandBuffer commit];
-
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
